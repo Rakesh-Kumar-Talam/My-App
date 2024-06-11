5491fccf72a41b362c9259d3679a8e0257bc17ad
rakesh <rakesh.22bce7907@vitapstudent.ac.in>
2024-06-11 13:09:24 +0530

    button click - log

--- app/src/main/java/com/example/myapp/MainActivity.kt
+++ app/src/main/java/com/example/myapp/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.myapp
 
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +18,10 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    fun Mybutton(view: View) {
+
+        Log.i("MainActivity", "Mybutton: button clicked")
     }
 }
