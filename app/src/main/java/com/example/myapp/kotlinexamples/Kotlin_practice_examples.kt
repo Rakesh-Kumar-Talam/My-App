792eb866e6031acf888c72bf7f1b67974e2bcc20
rakesh <rakesh.22bce7907@vitapstudent.ac.in>
2024-06-13 12:09:06 +0530

    Strings examples

---
+++ app/src/main/java/com/example/myapp/kotlinexamples/Kotlin_practice_examples.kt
@@ -0,0 +1,8 @@
+package com.example.myapp.kotlinexamples
+
+fun main() {
+    val numberOfFish = 5
+    val numberOfPlants = 12
+    println("I have $numberOfFish fish" + " and $numberOfPlants plants")
+    println("I have ${numberOfFish + numberOfPlants} fish and plants")
+}
