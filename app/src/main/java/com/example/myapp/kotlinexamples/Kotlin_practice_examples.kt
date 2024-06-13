16359fe04bcfacdac031e0d337b16c9e4f36a23b
rakesh <rakesh.22bce7907@vitapstudent.ac.in>
2024-06-13 12:11:57 +0530

    if-else and comparision example

--- app/src/main/java/com/example/myapp/kotlinexamples/Kotlin_practice_examples.kt
+++ app/src/main/java/com/example/myapp/kotlinexamples/Kotlin_practice_examples.kt
@@ -1,8 +1,18 @@
 package com.example.myapp.kotlinexamples
 
 fun main() {
+    //strings example
     val numberOfFish = 5
     val numberOfPlants = 12
-    println("I have $numberOfFish fish" + " and $numberOfPlants plants")
-    println("I have ${numberOfFish + numberOfPlants} fish and plants")
+    /*println("I have $numberOfFish fish" + " and $numberOfPlants plants")
+    println("I have ${numberOfFish + numberOfPlants} fish and plants")*/
+
+    //is-else and comparision examples
+    if (numberOfFish == 0) {
+        println("Empty tank")
+    } else if (numberOfFish < 40) {
+        println("Got fish!")
+    } else {
+        println("That's a lot of fish!")
+    }
 }
