d2245d9aab19d9bd0b09e669c67c2b17cdd90ac4
rakesh <rakesh.22bce7907@vitapstudent.ac.in>
2024-06-18 19:54:02 +0530

    recycler view part 3 - layout added

--- app/src/main/java/com/example/myapp/DataAdapter.kt
+++ app/src/main/java/com/example/myapp/DataAdapter.kt
@@ -1,9 +1,29 @@
 package com.example.myapp
 
+import android.util.Log
+import android.view.View
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 
-class DataAdapter: RecyclerView.Adapter<DataViewHolder>() {
-    class DataViewHolder {
+class DataAdapter: RecyclerView.Adapter<DataAdapter.DataViewHolder>() {
 
+    var TAG = DataAdapter::class.java.simpleName
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DataViewHolder {
+        Log.i(TAG, "He brought a row plank -- He gave it to prateek")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: DataViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    class DataViewHolder(rowPlank:View):RecyclerView.ViewHolder(rowPlank) {
+        init {
+            Log.i(TAG, "prateek is trying to find the text view in the row")
+        }
     }
 }
