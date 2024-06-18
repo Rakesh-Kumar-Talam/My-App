4dececd929f0b3690da3f3ba3c3b5f2033380819
rakesh <rakesh.22bce7907@vitapstudent.ac.in>
2024-06-18 13:19:06 +0530

    adding log to spinner and arrays.xml

--- app/src/main/java/com/example/myapp/Recycleview_Activity.kt
+++ app/src/main/java/com/example/myapp/Recycleview_Activity.kt
@@ -1,20 +1,41 @@
 package com.example.myapp
 
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Adapter
+import android.widget.AdapterView
+import android.widget.Spinner
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class Recycleview_Activity : AppCompatActivity() {
+class Recycleview_Activity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
+
+    var TAG = Recycleview_Activity::class.java.simpleName
+    lateinit var MySpinner: Spinner
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_recycleview)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        MySpinner = findViewById(R.id.spinner)
+        MySpinner.onItemSelectedListener = this
+
+        /*ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
+        }*/
+
+    }
+
+    override fun onItemSelected(adapter: AdapterView<*>, view: View?, position: Int, id: Long) {
+        var item = adapter?.selectedItem.toString()
+        Log.i(TAG,item)
+    }
+
+    override fun onNothingSelected(parent: AdapterView<*>?) {
+        TODO("Not yet implemented")
     }
 }
