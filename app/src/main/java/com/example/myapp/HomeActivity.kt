268e2620a63d00ea81e472d2058801d1d66bbc62
rakesh <rakesh.22bce7907@vitapstudent.ac.in>
2024-06-20 19:35:24 +0530

    entering text and displaying

--- app/src/main/java/com/example/myapp/HomeActivity.kt
+++ app/src/main/java/com/example/myapp/HomeActivity.kt
@@ -3,12 +3,15 @@ package com.example.myapp
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.EditText
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.myapp.R.id.activity_ex
+import com.example.myapp.R.id.tvui
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,7 +26,16 @@ class HomeActivity : AppCompatActivity() {
         var data = intent.extras?.getString("mykey")
         Log.i("HomeActivity","data is " +data)
         //Log.i("homeactivity",data)
-        var homeTextView:TextView = findViewById(activity_ex)
+        //var homeTextView:TextView = findViewById(activity_ex)
         //homeTextView.setText("data")
+    }
+
+    fun getShowText(view: View) {
+        //get the text from the edui
+        var Edui:EditText = findViewById(R.id.edui)
+        var textEntered = Edui.text.toString()
+        //show the text in tvui
+        var Tvui: TextView = findViewById(R.id.tvui)
+        Tvui.setText(textEntered)
     }
 }
