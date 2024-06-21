e2c6a7bafde03b7ffbd3f72ae04021e3975ef43b
rakesh <rakesh.22bce7907@vitapstudent.ac.in>
2024-06-21 17:37:32 +0530

    getting json part 1

--- app/src/main/java/com/example/myapp/HomeActivity.kt
+++ app/src/main/java/com/example/myapp/HomeActivity.kt
@@ -3,12 +3,15 @@ package com.example.myapp
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.myapp.R.id.activity_ex
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,5 +28,13 @@ class HomeActivity : AppCompatActivity() {
         //Log.i("homeactivity",data)
         var homeTextView:TextView = findViewById(activity_ex)
         //homeTextView.setText("data")
+    }
+
+    fun getJson(view: View) {
+        getMarsPhotos()
+    }
+
+    private fun getMarsPhotos() {
+        GlobalScope.launch {}
     }
 }
