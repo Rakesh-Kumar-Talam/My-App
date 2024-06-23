1744fa147dea74ceb1880ad337de2487cf50a2f7
rakesh <rakesh.22bce7907@vitapstudent.ac.in>
2024-06-23 17:14:46 +0530

    data binding

--- app/src/main/java/com/example/myapp/HomeActivity.kt
+++ app/src/main/java/com/example/myapp/HomeActivity.kt
@@ -10,6 +10,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import coil.load
@@ -29,6 +30,7 @@ class HomeActivity : AppCompatActivity() {
     //lateinit var recyclerview: RecyclerView
 
     private lateinit var binding: ActivityHomeBinding
+    var marsPhotoKotlin = MarsPhoto("999","moonimage.com")
 
     //lateinit var brake:Int?
     // lateinit var recyclerview:RecyclerView
@@ -38,12 +40,18 @@ class HomeActivity : AppCompatActivity() {
     //lateinit var imageView: ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        /*enableEdgeToEdge()
         //setContentView(R.layout.activity_home)
 
         binding = ActivityHomeBinding.inflate(layoutInflater)
         val view = binding.root
-        setContentView(view)
+        setContentView(view)*/
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_home)
+        binding.marsphotoxml = marsPhotoKotlin
+
+        /* var tvHome:TextView = findViewById(R.id.tvHome)
+         tvHome.setText(marsPhoto.imgSrc)*/
 
         //recyclerview = findViewById(R.id.recyclerView)
 
